8e5065a26e93ca313b240616da742d88c1a5afac
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-04-25 20:43:25 -0300

    cpuPlay Function done!

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+/**
+ * CLEAR SCORE
+ */
 let score = parseInt(localStorage.getItem("score")) || 0;
 
 function clearScore() {
@@ -10,3 +13,17 @@ function clearScore() {
 const clearScoreButton = document.querySelector("#clearScore");
 clearScoreButton.addEventListener("click", clearScore);
 
+/**
+ * UPDATE SCORE
+ */
+function updateScore() {
+  document.getElementById('score').textContent = score;
+}
+
+/**
+ * CPU PLAY
+ */
+function cpuPlay() {
+  const options = ['rock', 'paper', 'scissors'];
+  return options[Math.floor(Math.random() * options.length)];
+}
